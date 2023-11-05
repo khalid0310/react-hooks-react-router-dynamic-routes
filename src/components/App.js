d9fce9e837e2a88b8125c4395b72ea5d787e13ee
khalid <khalid.mohamed@moringaschool.com>
2023-11-05 11:16:41 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import MoviesPage from "./MoviesPage";
 
+
 function App() {
   const [movies, setMovies] = useState([
     { id: 1, title: "A River Runs Through It" },
